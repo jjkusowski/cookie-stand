7b4723b7162eb0394d08711a92f35369595b97bf
jjkusowski <jjkusowski@yahoo.com>
2017-09-26 14:26:25 -0700

    added array of cookies per hour to the constructor

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 'usestrict';
 
+//This is the hours that all stores are open
 var hours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
 
+//This array will hold all constructed store objects
 var allStores = [];
 
+// Gives access to the table on the DOM
+var cookieTable = document.getElementById('cookies');
+
+// Store constructor function
 function Store(name, minCust, maxCust, cookCust) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.cookCust = cookCust;
+  this.cookHourArray = [];
+  this.calcCookHour = function() {
+    return Math.ceil((Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)) * this.cookCust);
+  };
+
+  this.calcCookArray = function() {
+    for (var i = 0; i < hours.length; i++) {
+      var temp = this.calcCookHour();
+      console.log('cookies for one hour at ' + this.name + ' is :' + temp);
+      this.cookHourArray.push(temp);
+    }
+  };
+  this.calcCookArray();
+
   allStores.push(this);
 }
 
+
+
+// Add stores
 new Store('1st and Pike', 23, 65, 6.3);
 new Store('Seatac Airport', 3, 24, 1.2);
 
